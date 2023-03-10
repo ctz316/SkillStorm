2f00168ebed277189fbd6a3d1a8fe536b0ba739e
Kid Barker <ctz316.jobs@gmail.com>
2023-03-10 12:16:25 -0700

    Adding week 2 day 5

--- SkillStorm/src/ProjectsForClass/week_02/Day_3_Interest_Calc_Redone.java
+++ SkillStorm/src/ProjectsForClass/week_02/Day_3_Interest_Calc_Redone.java
@@ -5,67 +5,70 @@ import java.util.Scanner;
 public class Day_3_Interest_Calc_Redone {
         public static void main(String[] args) {
 
-            // Reading Input to see which calculator to use//
-            System.out.println("Will this be for simple interest or compound interest? type 'S' for simple and 'C' for compound interest: ");
-            Scanner in = new Scanner(System.in);
-            String answer = in.next();
+            // Setting variable
+                String answer;
 
-            if (answer.equalsIgnoreCase("S")) {
-                simpleInterest();
-            }
-            else if (answer.equalsIgnoreCase("C")) {
-                compoundInterest();
-            }
-            else {
-                System.out.println("Invalid entry. Please restart the program.");
-            }
+            // Do While loop to verify user input is correct
+                do {
+                    // Reading Input to see which calculator to use//
+                        System.out.println("Will this be for simple interest or compound interest? type 'S' for simple and 'C' for compound interest: ");
+                        Scanner in = new Scanner(System.in);
+                        answer = in.next();
+                } while ((!answer.equalsIgnoreCase("S")) && (!answer.equalsIgnoreCase("C"))) ;
+
+            // Switching to correct calculations based on user input
+                if (answer.equalsIgnoreCase("S")) {
+                    simpleInterest();
+                } else if (answer.equalsIgnoreCase("C")) {
+                    compoundInterest();
+                }
 
         }
 
         private static String simpleInterest() {
 
             // Declaring Variables
-            double userOriginalAmount;
-            double userInterestRate;
-            int userNumberOfYears;
+                double userOriginalAmount;
+                double userInterestRate;
+                int userNumberOfYears;
 
             // Getting user info
-            System.out.print("Enter the amount: ");
-            Scanner in = new Scanner(System.in);
-            userOriginalAmount = in.nextDouble();
-            System.out.print("Enter the interest rate: ");
-            userInterestRate = in.nextDouble();
-            System.out.print("Enter the number of years: ");
-            userNumberOfYears = in.nextInt();
-            in.close();
+                System.out.print("Simple Interest Calculator: Enter the amount: ");
+                Scanner in = new Scanner(System.in);
+                userOriginalAmount = in.nextDouble();
+                System.out.print("Enter the interest rate: ");
+                userInterestRate = in.nextDouble();
+                System.out.print("Enter the number of years: ");
+                userNumberOfYears = in.nextInt();
+                in.close();
 
             // Calculating Interest //
-            double interestAccrued = (userOriginalAmount * (1 + ((userInterestRate * .01) * userNumberOfYears)) - userOriginalAmount);
+                double interestAccrued = (userOriginalAmount * (1 + ((userInterestRate * .01) * userNumberOfYears)) - userOriginalAmount);
 
             // Printing Results //
-            return System.out.printf("The interest accrued is: $%.2f%n", interestAccrued).toString(); // Printing the interest accrued in a correct format
+                return System.out.printf("The interest accrued is: $%.2f%n", interestAccrued).toString(); // Printing the interest accrued in a correct format
         }
 
         private static String compoundInterest() {
 
             // Creating Variables
-            double userOriginalAmount;
-            double userInterestRate;
-            int userNumberOfMonths;
-            double interestAccrued;
+                double userOriginalAmount;
+                double userInterestRate;
+                int userNumberOfMonths;
+                double interestAccrued;
 
             // Reading Input //
-            System.out.print("Enter the amount: ");
-            Scanner in = new Scanner(System.in);
-            userOriginalAmount = in.nextDouble();
-            System.out.print("Enter the interest rate: ");
-            userInterestRate = in.nextDouble();
-            System.out.print("Enter the number of Months: ");
-            userNumberOfMonths = in.nextInt();
-            in.close();
+                System.out.print("Compound Calculator: Enter the amount: ");
+                Scanner in = new Scanner(System.in);
+                userOriginalAmount = in.nextDouble();
+                System.out.print("Enter the interest rate: ");
+                userInterestRate = in.nextDouble();
+                System.out.print("Enter the number of Months: ");
+                userNumberOfMonths = in.nextInt();
+                in.close();
 
             // Calculating Interest //
-            interestAccrued = (userOriginalAmount * Math.pow(1 + ((userInterestRate * .01) / 12), userNumberOfMonths)) - userOriginalAmount;
+                interestAccrued = (userOriginalAmount * Math.pow(1 + ((userInterestRate * .01) / 12), userNumberOfMonths)) - userOriginalAmount;
 
             // Printing Results //
                 return System.out.printf("The interest accrued is: $%.2f%n", interestAccrued).toString();
